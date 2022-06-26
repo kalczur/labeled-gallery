c115576882c8be41219561ee6b838afeb3231b3b
kalczur <kalczur@gmail.com>
2022-06-26 11:19:46 +0200

    LG-1 Create an ASP.NET project

---
+++ LabeledGallery/Startup.cs
@@ -0,0 +1,24 @@
+﻿namespace LabeledGallery;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration, IWebHostEnvironment env)
+    {
+        Configuration = configuration;
+        HostingEnvironment = env;
+    }
+
+    public IConfiguration Configuration { get; }
+    
+    private IWebHostEnvironment HostingEnvironment { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        var settings = new Settings();
+        Configuration.Bind(settings);
+    }
+    
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+    {
+    }
+}
